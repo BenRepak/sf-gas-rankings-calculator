19890d663e59574028af604f8be3f0af57c4a7ef
Ben Repak <benrepak@gmail.com>
2021-12-15 15:51:55 -0700

    updated script toe send email

---
+++ force-app/main/default/lwc/bSEnrollmentButton/bSEnrollmentButton.js
@@ -0,0 +1,12 @@
+import { api, LightningElement } from 'lwc';
+
+
+export default class Test_Visitor_Authentication extends LightningElement {
+
+    @api conQualtricsURL;
+    @api varBase64EncodedString;
+    @api url;
+    @api linkText;
+    
+     
+}
